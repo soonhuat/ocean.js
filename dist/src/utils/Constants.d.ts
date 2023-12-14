c6a81c6f5a30f5dd6806c7fa641db711336c2db0
Soon Huat <soonhuat.phan@hotmail.com>
2023-12-14 14:59:21 +0800

    built - private edge gas

--- dist/src/utils/Constants.d.ts
+++ dist/src/utils/Constants.d.ts
@@ -1,4 +1,7 @@
+import { ethers } from 'ethers';
 export declare const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 export declare const GASLIMIT_DEFAULT = 1000000;
+export declare const GASLIMIT = 8000000;
+export declare const GAS_PRICE: ethers.BigNumber;
 export declare const MAX_UINT_256 = "115792089237316195423570985008687907853269984665640564039457584007913129639934";
 export declare const FEE_HISTORY_NOT_SUPPORTED = "Returned error: Method eth_feeHistory not supported.";
